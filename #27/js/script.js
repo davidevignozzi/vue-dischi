159437803303416bd34ee41451a2d43cd641d34c
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-04-08 19:06:51 +0200

    Bonus 1

--- #27/js/script.js
+++ #27/js/script.js
@@ -3,23 +3,42 @@ var app = new Vue({
     el : '#app',
 
     data: {
-      cds : [],
-      index : 0,
+        cds : [],
+        index : 0,
+        genreList: [],
+        selected: "All",
     },
     
     methods: {
-        // methods
+
+        // pusho tutti i generi in un array per stamparli nella select
+        genreOption() {
+            const self = this;
+            this.cds.forEach((el) => {
+              if (!self.genreList.includes(el.genre)) {
+                self.genreList.push(el.genre);
+              }
+            });
+        },
+
+        filtroList: function () {
+            this.cds.forEach((element) => {
+              if (element.includes(this.selected)) {
+                return { ...element};
+                }
+            });
+        },
+
     },
 
     mounted(){
-      const self = this;
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music').
-          then(function(resp){
-            self.cds = resp.data.response;
-          })
-      console.log(self.cds);
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((resp) => {
+          this.cds = resp.data.response;
+          this.genreOption();
+        });
     },
 
-  })
-  
-  Vue.config.devtools = true
+})
+Vue.config.devtools = true
